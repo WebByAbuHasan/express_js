dab925a8826afc71c24eb0b9ed88a33a7abc29b9
MD ABU HASAN <abuhasan43098@gmail.com>
2025-01-03 21:47:59 +0600

    routing

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const app= express();
 
 
 app.get("/",(req,res)=>{
-    res.send("Hello World!");
+    res.send("this is home page");
 })
-
+app.get("/about",(req,res)=>{
+    res.send("this is about page");
+})
+app.get("/contact",(req,res)=>{
+    res.send("this is contact page");
+})
+app.get("/blog",(req,res)=>{
+    res.send("this is blog page");
+})
 
 app.listen(8080,function(){
     console.log("Server running on port 8080");
